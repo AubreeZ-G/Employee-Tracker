361dec19c6a3a52b4e4917602a7111d41250ff68
Aubree Zarges <aubreezarges@yahoo.com>
2022-07-20 17:02:02 -0600

    dpne

--- db/queries.sql
+++ db/queries.sql
@@ -5,3 +5,5 @@ select * from department;
 select * from roles;
 
 select * from employee;
+
+select r.id, r.title,d.name from roles r , department d  where r.department_id = d.id;
--- index.js
+++ index.js
@@ -79,3 +79,79 @@ function viewRoles() {
         startApp()
     })
 }
+function addDepartment() {
+    inquirer.prompt([
+        {
+            type:"input",
+            message:"Enter department name?",
+            name:"dname"
+        }
+    ]).then(response => {
+    db.query("insert into department(name) values (?)",response.dname, function (err, data) {
+        if (err) throw err;
+        console.table(data)
+        startApp()
+    })
+})
+}
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type:"input",
+            message:"Enter employee first name",
+            name:"fname"
+        },
+        {
+            type:"input",
+            message:"Enter employee last name",
+            name:"lname"
+        },
+        {
+            type:"list",
+            message:"Enter employee role id",
+            name:"id",
+            choices:[
+                { value:1,name:"Manager-Marketing"},
+                {value:2,name:"Manager-Sales"},
+                {value:3,name:"Manager-HR"},
+                {value:4,name:"Manager-Developers"},]
+        },
+    ]).then(response => {
+    db.query("insert into employee(first_name,last_name,role_id) values(?,?,?)",
+    [response.fname,response.name,response.id], function (err, data) {
+        if (err) throw err;
+        console.table(data)
+        startApp()
+    })
+})
+}
+function addRole() {
+    inquirer.prompt([
+        {
+            type:"input",
+            message:"Enter role title",
+            name:"title"
+        },
+        {
+            type:"input",
+            message:"Enter role salary",
+            name:"salary"
+        },
+        {
+            type:"input",
+            message:"Enter department id",
+            name:"depatment_id",
+            choices:[
+                { value:1,name:"Marketing"},
+                {value:2,name:"Sales"},
+                {value:3,name:"HR"},
+                {value:4,name:"Developers"},]
+        },
+    ]).then(response => {
+    db.query("insert into roles(title,salary,department_id) values (?,?,?)",[response.title,response.salary,response.department_id], function (err, data) {
+        if (err) throw err;
+        console.table(data)
+        startApp()
+    })
+})
+}
